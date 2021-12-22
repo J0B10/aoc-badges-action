119a149cdfd63d7972245a2c43d7fe1c97f6bba8
Jonas <17405009+J0B10@users.noreply.github.com>
2021-12-22 08:58:44 +0100

    Refactoring to silence pycharm

--- aoc-badges.py
+++ aoc-badges.py
@@ -10,50 +10,50 @@ year = os.getenv('INPUT_YEAR')
 leaderboard = os.getenv('INPUT_LEADERBOARD')
 session = os.getenv('INPUT_SESSION')
 readme = os.getenv('INPUT_FILE')
-id = os.getenv('INPUT_USERID')
+userid = os.getenv('INPUT_USERID')
 day_regex = os.getenv('INPUT_DAYREGEX')
 stars_regex = os.getenv('INPUT_STARSREGEX')
 days_completed_regex = os.getenv('INPUT_DAYSCOMPLETEDREGEX')
-if year is None or not year :
-  year = date.today().year
+if year is None or not year:
+    year = date.today().year
 else:
-  try:
-    year = int(year)
-  except ValueError:
-    print('year input is not an integer')
-    exit(1)
-if leaderboard is None or not leaderboard :
-  leaderboard = f'https://adventofcode.com/{year}/leaderboard/private/view/{id}.json'
+    try:
+        year = int(year)
+    except ValueError:
+        print('year input is not an integer')
+        exit(1)
+if leaderboard is None or not leaderboard:
+    leaderboard = f'https://adventofcode.com/{year}/leaderboard/private/view/{userid}.json'
 
 # fetch stars
-cookie = { 'session' : session }
+cookie = {'session': session}
 print('Fetching leaderboard data from : ' + leaderboard)
-r = requests.get(leaderboard, cookies = cookie)
+r = requests.get(leaderboard, cookies=cookie)
 if r.status_code != 200:
-  print(f'Leaderboard API returned status code {r.status_code}: {r.text}')
-  exit(1)
+    print(f'Leaderboard API returned status code {r.status_code}: {r.text}')
+    exit(1)
 try:
-  data = json.loads(r.text)
+    data = json.loads(r.text)
 except json.JSONDecodeError as err:
-  print('Could not parse leaderboard json. Is the leaderboard url correct & your session code valid?')
-  print(err)
-  exit(1)
-stars = data['members'][id]['stars']
+    print('Could not parse leaderboard json. Is the leaderboard url correct & your session code valid?')
+    print(err)
+    exit(1)
+stars = data['members'][userid]['stars']
 
 # completed days
 days_completed = 0
-for day in data['members'][id]['completion_day_level']:
-  if '2' in data['members'][id]['completion_day_level'][day]:
-    days_completed += 1
+for day in data['members'][userid]['completion_day_level']:
+    if '2' in data['members'][userid]['completion_day_level'][day]:
+        days_completed += 1
 
 # current day
 today = date.today() - timedelta(hours=5)
 if today < date(year, 12, 1):
-  day = 0
+    day = 0
 elif today > date(year, 12, 31):
-  day = 24
+    day = 24
 else:
-  day = today.day
+    day = today.day
 
 print(f'Day: {day}')
 print(f'Stars: {stars}')
@@ -64,7 +64,7 @@ f = io.open(readme, mode='r', encoding='utf-8')
 txt = f.read()
 f.close()
 
-#replace values
+# replace values
 txt = re.sub(day_regex, str(day), txt)
 txt = re.sub(stars_regex, str(stars), txt)
 txt = re.sub(days_completed_regex, str(days_completed), txt)
